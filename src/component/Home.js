1bdb00f3766a8a46275ebbd52c688b8e91537718
Laxman Kashidkar <kashidkar37@gmail.com>
2022-10-30 21:53:21 +0530

    added cv

--- src/component/Home.js
+++ src/component/Home.js
@@ -391,7 +391,7 @@ export default function Example() {
                                         <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                                             <div className="rounded-md shadow">
                                                 <a
-                                                    href="!#"
+                                                    href="./laxman_kashidkar_resume.pdf"
                                                     className="w-full flex items-center justify-center px-5 py-2 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
                                                 >
                                                     Download CV
